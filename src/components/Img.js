7a7e40459fb9ff2c03718d0cb55e9d000c6b26ff
dimanklg <dimankaliningrad@gmail.com>
2016-08-27 10:40:52 +0100

    add img

--- src/components/Img.js
+++ src/components/Img.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
 
 var Img = React.createClass({
+
+
   render(){
+    /**
+     * [margin for first element and next]
+     * @return {[String]} [margin]
+     */
+    let marginLeft = ()=>{
+       return (this.props.key==0)?'0':'14px'
+      }
+    /**
+     * [style object for img]
+     * @type {Object}
+     */
     let imgStyle={
       height: this.props.settings.list.picHeight,
       float: 'left',
       marginLeft: '14px',
-      cursor: 'pointer'
-    }
-    let firstImgStyle ={
-      height:  this.props.settings.list.picHeight,
-      float: 'left',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      marginLeft: marginLeft()
     }
     return (
     <div>
       <img 
-        style={(this.props.keyD==0)?firstImgStyle:imgStyle} 
+        style={imgStyle} 
         src={this.props.srcData.img}
         onClick={this.props.imgSelect}/>
+        {this.props.key}
     </div>
     )
   }
