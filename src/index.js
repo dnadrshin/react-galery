5f8ea287967e4841a10fdade1f4c1299d1f5f8b9
dimanklg <dimankaliningrad@gmail.com>
2016-09-07 21:56:22 +0200

    add redux. add new branch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import { App } from './App';
+import reducer from './reducer';
 
+
+const store = createStore(reducer);
 
 render(<App />, document.getElementById('root'));
